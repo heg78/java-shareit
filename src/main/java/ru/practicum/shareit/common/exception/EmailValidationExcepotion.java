e3f06378b8edc6f58ce25fca35d9e198ddb1ce35
heg78 <heg78@ymail.ru>
2024-04-21 20:30:09 +0300

    review:устранить небольшие замечания

--- src/main/java/ru/practicum/shareit/common/exception/EmailValidationExcepotion.java
+++ src/main/java/ru/practicum/shareit/common/exception/EmailValidationExcepotion.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.common.exception;
 
-public class EmailValidationExcepotion extends RuntimeException{
+public class EmailValidationExcepotion extends RuntimeException {
     public EmailValidationExcepotion(String message) {
         super(message);
     }
