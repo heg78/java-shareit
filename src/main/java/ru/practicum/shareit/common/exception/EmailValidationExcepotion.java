67b5d73784e56005a524f1b11de6c1938ec02687
heg78 <heg78@ymail.ru>
2024-04-30 20:11:05 +0300

    feat: add docker

--- src/main/java/ru/practicum/shareit/common/exception/EmailValidationExcepotion.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.common.exception;
-
-public class EmailValidationExcepotion extends RuntimeException {
-    public EmailValidationExcepotion(String message) {
-        super(message);
-    }
-}
