199ecfae95cfe814c8806e077f578a8d37f6d097
heg78 <heg78@ymail.ru>
2024-05-04 18:34:25 +0300

    feat: add docker

--- server/src/main/java/ru/practicum/shareit/item/ItemService.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -16,7 +16,6 @@ import ru.practicum.shareit.user.model.User;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
